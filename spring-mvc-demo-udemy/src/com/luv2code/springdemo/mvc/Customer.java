6d1bab0bb345febc160ae853ca60b78daf9a9f06
Eduardo Tanabe <e_tanabe@yahoo.com.br>
2021-01-23 21:59:21 -0400

    Modificação para aceitar null no campo "Free passes" e customização de mensagem de erro quando não for número inteiro no referido campo

--- spring-mvc-demo-udemy/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring-mvc-demo-udemy/src/com/luv2code/springdemo/mvc/Customer.java
@@ -14,9 +14,15 @@ public class Customer {
 	@Size(min = 1, message = "is required")
 	private String lastName;
 	
+	@NotNull(message = "is required")
 	@Min(value = 0, message = "must be greater than or equal to zero")
 	@Max(value = 10, message = "must be less than or equal to 10")
-	private int freePasses;
+	private Integer freePasses;  // foi modificado de int para Integer para aceitar null
+	
+	// quando adicionamos um valor inválido, como "jdkafjaljaklfj", no campo "Free passes" da página web, aparecerá uma mensagem gigante de erro.
+	// Para contornar isso, foi adicionado uma mensagem customizada. Assim, foi necessário nesta aula (aual 160 do curso) a pasta resources dentro
+	// da pasta src, adicionar o arquivo messages.properties com a mensagem customizada no interior da pasta criada e modificar o arquivo
+	// spring-mvc-demo-servlet.xml q está na pasta WEB-INF	
 	
 	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")   // o 5 do regexp refere-se a quantidade de caracteres q aceita
 	private String postalCode;
@@ -36,12 +42,12 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
-	public int getFreePasses() {
+	
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 
